00bcd812a807447276446718031ad97b332e4091
Jeff Pollard <jeff.pollard@gmail.com>
2011-02-20 21:00:47 -0800

    Add stub matcher.  Will work on specs later

--- lib/rspec-encoding-matchers.rb
+++ lib/rspec-encoding-matchers.rb
@@ -1,7 +1,7 @@
-module Rspec
-  module Encoding
-    module Matchers
-      # Your code goes here...
-    end
+module RSpecEncodingMatchers
+  extend RSpec::Matchers::DSL
+
+  matcher :encoded_as do |expected|
+    match { |actual| actual.encoding == expected }
   end
 end

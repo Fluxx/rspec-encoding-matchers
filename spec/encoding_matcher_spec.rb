434896841226bc727d17a942b91d776a7df229d7
Jeff Pollard <jeff.pollard@gmail.com>
2011-02-22 22:32:19 -0800

    Add specs for should_not

--- spec/encoding_matcher_spec.rb
+++ spec/encoding_matcher_spec.rb
@@ -4,16 +4,32 @@ require "spec_helper"
 
 describe "#be_encoded_as" do
 
-  it "matches encoding of strings with official names" do
-    "hello".should be_encoded_as("UTF-8")
+  describe "should" do
+
+    it "matches encoding of strings with official names" do
+      "hello".should be_encoded_as("UTF-8")
+    end
+  
+    it "matches encoding of strings alised to the actual encoding" do
+      "hello".should be_encoded_as("CP65001")
+    end
+
+    it "matches encodings found with symbols" do
+      "hello".should be_encoded_as(:CP65001)
+    end
+
   end
   
-  it "matches encoding of strings alised to the actual encoding" do
-    "hello".should be_encoded_as("CP65001")
-  end
-  
-  it "matches encodings found with symbols" do
-    "hello".should be_encoded_as(:CP65001)
+  describe "should_not" do
+
+    it "matches encoding of strings with official names" do
+      "hello".should_not be_encoded_as("ASCII")
+    end
+
+    it "matches encodings found with symbols" do
+      "hello".should_not be_encoded_as(:ASCII)
+    end
+
   end
     
 end
@@ -22,14 +38,26 @@ Encoding.list.each do |encoding|
   encoding.names.each do |name|
     
     matcher_name = "be_#{name.gsub("-", "_").downcase}_encoded"
-    
+
     describe "##{matcher_name}" do
-      
-      it "matches stings in the requested encoding" do
-        "hello".encode(encoding).should send(matcher_name) unless encoding.dummy?
+
+      describe "should" do
+
+        it "matches stings in the requested encoding" do
+          "hello".encode(encoding).should send(matcher_name) unless encoding.dummy?
+        end
+
       end
-      
+
+      describe "should_not" do
+
+        it "matches stings in the requested encoding" do
+          "hello".should_not send(matcher_name) unless encoding == Encoding.find("UTF-8")
+        end
+
+      end
+
     end
-    
+
   end
 end
